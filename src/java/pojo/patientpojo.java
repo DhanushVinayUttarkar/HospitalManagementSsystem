009d23f039fbacedd464f3372ef242eac0bcead3
DragonlordDrake <vudanny4287537@gmail.com>
2017-11-13 21:26:40 +0530

    Add files via upload

---
+++ src/java/pojo/patientpojo.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pojo;
+
+/**
+ *
+ * @author matlab
+ */
+public class patientpojo {
+    
+    int p_id;
+    String p_name;
+    String p_wimage;
+
+    public int getP_id() {
+        return p_id;
+    }
+
+    public void setP_id(int p_id) {
+        this.p_id = p_id;
+    }
+
+    public String getP_name() {
+        return p_name;
+    }
+
+    public void setP_name(String p_name) {
+        this.p_name = p_name;
+    }
+
+    public String getP_wimage() {
+        return p_wimage;
+    }
+
+    public void setP_wimage(String p_wimage) {
+        this.p_wimage = p_wimage;
+    }
+    
+    
+    
+}
